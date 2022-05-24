69007c0087b587f33d7f47fffca8a40e0ed09ded
david.daso <david.daso@sterling.ng>
2022-05-24 14:54:05 +0100

    pop and unshift

--- DoublyLinkedList.js
+++ DoublyLinkedList.js
@@ -30,4 +30,38 @@ class DoublyLinkedList{
         this.length ++
         return this
     }
+    pop(){
+        if(this.length === 0) return undefined;
+        // let pre = this.tail.prev 
+        // this.tail = pre 
+        // this.tail.next = null
+        let temp = this.tail
+        
+        
+        if(this.length === 1){
+            this.head =  null
+            this.tail = null
+        }else{
+            this.tail = this.tail.prev
+            this.tail.next = null
+            temp.prev = null
+        }
+        this.length--
+        return temp
+    }
+    unshift(value){
+        const newNode = new Node(value);
+        if(!this.head){
+            this.head = newNode
+            this.tail = newNode
+        }
+        let temp = this.head
+        this.head = newNode
+        this.head.next = temp
+        temp.prev = newNode
+
+        this.length++
+        return this
+    }
+    
 }
