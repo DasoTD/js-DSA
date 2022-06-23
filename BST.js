c81e3c1ff302edb8e518997c06705deb807ab66c
David Daso <daso.timileyin@gmail.com>
2022-06-23 15:00:40 +0100

    BFS

--- BST.js
+++ BST.js
@@ -49,6 +49,21 @@ class Bst{
         }
         return false
     }
+
+    BFS(){
+        let currentNode = this.root
+        let results = [];
+        let queue = [];
+        queue.push(currentNode)
+
+        while(queue.length){
+            currentNode = queue.shift()
+            results.push(currentNode.value)
+            if(currentNode.left) queue.push(currentNode.left)
+            if(currentNode.right) queue.push(currentNode.right)
+        }
+        return results
+    }
 }
 let myBst = new Bst(40)
 myBst
